dac6a1060dd484551ac7f9dca153ec40c88f44e9
Gerry <864241986@qq.com>
2018-12-20 17:47:38 +0800

    Add LinkedHashMap Demo

---
+++ src/LinkedHashMapDemo.java
@@ -0,0 +1,14 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author:Gerry
+ * @description:
+ * @date: Created in 2018/12/13
+ */
+public class LinkedHashMapDemo {
+    public static void main(String[] args) {
+        LinkedHashMap<String,String> accessOrderedMap=new LinkedHashMap<>(16,0.75F,true);
+
+    }
+}
